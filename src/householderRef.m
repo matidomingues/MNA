a32f4c29d60d70b4cb327c1ee33ec39c9649ca58
mannias <mannias@gmail.com>
2014-05-07 23:58:06 -0300

    qr

--- src/householderRef.m
+++ src/householderRef.m
@@ -1,12 +1,6 @@
 function H_u = householderRef(v,k)
-	u=zeros(rows(v),1); 
-	s=sign(v(k))*norm(v); 
-	for i=1:rows(v)
-		if(i==k)
-			u(i)=v(i)+s;
-		else
-			u(i)=v(i);
-		endif 
-	end
-	H_u = eye(rows(v))-((2*u*u')/(norm(u)^2)); 
+	u=v;
+	u(k)=u(k)+sign(v(k))*norm(v);
+	u_norm=u/norm(u);
+	H_u=eye(rows(v))-2*u_norm*u_norm';
 end
--- src/solveQR.m
+++ src/solveQR.m
@@ -1,14 +1,13 @@
 function [Q,R] = solveQR(A)
-	m=rows(A);
-	n=columns(A); 
-	R_accum=A; 
-	Q_accum=eye(m); 
-	for i=1:n-1
-		H=eye(m); 
-		H(i:m,i:m)=householderRef(R_accum(i:m,i),1);
-		Q_accum=Q_accum*H'; 
-		R_accum=H*R_accum;
-	end 
-	R=R_accum; 
-	Q=Q_accum;
+        [m,n]=size(A);
+
+        R=A;
+        Q=eye(m);
+
+        for i=1:n
+            H = eye(m);
+            H(i:end,i:end)=householderRef(R(i:end,i),1);
+            Q(:,i:end)=Q*H'(:,i:end);
+            R(i:end,:)=H(i:end,:)*R;
+        end
 end
