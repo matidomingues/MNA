9a57f2a7eea5f56d956f8b3e040ba7269c420642
lulirez <lulirez@gmail.com>
2014-05-09 18:17:54 -0300

    asdf

--- src/execute.m
+++ src/execute.m
@@ -7,18 +7,31 @@ function ans = execute(data,f)
         if f == 0
             [Q,R] = solveQRGS(A);
             X(i-3, :) = inv(R) * Q' * y;
+             [k,m] = histogram(y,A,X(i-3, :));
+             ans.(char('@' + i)).x = k;
+             ans.(char('@' + i)).m = m;
+             
         elseif f == 1
             B = A' * A;
             G = cholesky(B);
             z0 = inv(G) * A' * y;
             x0 = inv(G') * z0;
             X(i-3, :) = x0;
+             [k,m] = histogram(y,A,X(i-3, :));
+             ans.(char('@' + i)).x = k;
+             ans.(char('@' + i)).m = m;
+             
         else
             X(i-3, :) = A\y;
             %Este metodo es el super optimo de matlab
+             [k,m] = histogram(y,A,X(i-3, :));
+             ans.(char('@' + i)).x = k;
+             ans.(char('@' + i)).m = m;
+             
+              
         end
-        err(i)=calculateError(A,X(i-3, :),y);
-        ans.(char('@' + i)) = histogram(X(i-3, :),y);
+         err(i)=calculateError(A,X(i-3, :),y);
+       
     end
     err = err(:,4:end);
     ans.err = err;
--- src/histogram.m
+++ src/histogram.m
@@ -1,14 +1,3 @@
-function [nn,xx] = histogram(vars,y)
-    
-    n = size(vars,1);
-    vels = getVelocity( vars );
-    error=zeros(n,1);
-    
-    for i=1:n    
-        
-        error(i)=norm(y(i)-vels(i))^2;
-        disp(error(i));
-    end
-    
-    [nn,xx]=hist(error);
+function [nn,xx] = histogram(y,A,b)
+   	[nn,xx]=hist(abs(y-A*b'));
 end
